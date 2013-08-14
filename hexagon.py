eab33f6c8320329f4627ec0f1363841feeef3ac8
RedSpaceman <samuel.corcoran+github@gmail.com>
2013-08-14 09:44:26 +0100

    Hexagon objects contain point information to remove the need for repeated calculations Drawing of a hex grid fitted to the width of the screen is now implemented

--- hexagon.py
+++ hexagon.py
@@ -1,10 +1,52 @@
-
+import pyglet
+from itertools import chain
 
 class Hexagon():
 	def __init__(self, centrePoint=(0,0), radius=20):
 		self.centre = centrePoint
 		self.radius = radius
+		self.points = self.calculatePoints()
+	
+	def calculatePoints(self):
+		sqrtThree = 1.73205080757
+		innerRadius = (sqrtThree*self.radius)/2
+		points=[]
+		#N, Top point
+		points.append((self.centre[0], self.centre[1]+self.radius))
+		#NE
+		points.append((self.centre[0]+innerRadius, self.centre[1]+(self.radius/2)))
+		#SE
+		points.append((self.centre[0]+innerRadius, self.centre[1]-(self.radius/2)))
+		#S
+		points.append((self.centre[0], self.centre[1]-self.radius))
+		#SW
+		points.append((self.centre[0]-innerRadius, self.centre[1]-(self.radius/2)))
+		#NW
+		points.append((self.centre[0]-innerRadius, self.centre[1]+(self.radius/2)))
+		return points
 
-	def getPerimeterCoords(self):
-		topPointX = self.centre[0]
-		topPointY = self.centre[1]
+	def drawHex(self, fullHex=True, edgeColor=(1.0,0.0,0.0,1.0)):
+		pointsList = []
+		pointsList = list(chain.from_iterable(self.points))
+		numEdges = 6
+		if not fullHex:
+			numEdges = 4
+			pointsList = pointsList[:numEdges*2]
+			# Draw edges
+			pyglet.gl.glColor4f(*edgeColor)
+			# Line strip instead of loop
+			pyglet.graphics.draw(numEdges, pyglet.gl.GL_LINE_STRIP,
+				('v2f', pointsList)
+			)
+		else:
+			# Draw edges
+			pyglet.gl.glColor4f(*edgeColor)
+			# Line loop to reduce vertex list requirements
+			pyglet.graphics.draw(numEdges, pyglet.gl.GL_LINE_LOOP,
+				('v2f', pointsList)
+			)
+		# Draw points
+		pyglet.gl.glColor4f(0.0,1.0,0.0,1.0)
+		pyglet.graphics.draw(numEdges, pyglet.gl.GL_POINTS,
+			('v2f', pointsList)
+		)
--- main.py
+++ main.py
@@ -6,6 +6,9 @@ import random
 import math
 
 window = pyglet.window.Window()
+screenWidth = 800
+screenHeight = 600
+window.set_size(800, 600)
 image = pyglet.resource.image('kitten.jpg')
 
 gridLength = 10
@@ -26,42 +29,40 @@ for i in range(6):
 #    	('v2i', (250, 250))
 #)
 
-def drawHex(fullHex=True, startPoint=(400,400), radius=50):
+def drawHex(fullHex=True, centrePoint=(400,400), radius=50, innerRadius=False, edgeColor=(0.0,0.0,1.0,1.0)):
 	edgeLength = radius
-	sqrtThree = 1.73205080757
-	innerRadius = (sqrtThree*radius)/2
+	if not innerRadius:
+		sqrtThree = 1.73205080757
+		innerRadius = (sqrtThree*radius)/2
 	points=[]
 	#Top point (North)
-	points.append(startPoint[0])
-	points.append(startPoint[1]+radius)
+	points.append(centrePoint[0])
+	points.append(centrePoint[1]+radius)
 	#NE
-	points.append(startPoint[0]+innerRadius)
-	points.append(startPoint[1]+(edgeLength/2))
+	points.append(centrePoint[0]+innerRadius)
+	points.append(centrePoint[1]+(edgeLength/2))
 	#SE
-	points.append(startPoint[0]+innerRadius)
-	points.append(startPoint[1]-(edgeLength/2))
+	points.append(centrePoint[0]+innerRadius)
+	points.append(centrePoint[1]-(edgeLength/2))
 	#S
-	points.append(startPoint[0])
-	points.append(startPoint[1]-radius)
-
+	points.append(centrePoint[0])
+	points.append(centrePoint[1]-radius)
 	# FullHex draws all six sides, otherwise only three are drawn
 	# Missing out three sides reduces draws for gridded hexagons where edges overlap
 	numEdges = 4
 	if fullHex:
 		#SW
-		points.append(startPoint[0]-innerRadius)
-		points.append(startPoint[1]-(edgeLength/2))
+		points.append(centrePoint[0]-innerRadius)
+		points.append(centrePoint[1]-(edgeLength/2))
 		#NW
-		points.append(startPoint[0]-innerRadius)
-		points.append(startPoint[1]+(edgeLength/2))
+		points.append(centrePoint[0]-innerRadius)
+		points.append(centrePoint[1]+(edgeLength/2))
 		numEdges = 6
-
-	#Draw edges
-	pyglet.gl.glColor4f(0.0,0.0,1.0,1.0)
-	pyglet.graphics.draw(4, pyglet.gl.GL_LINE_STRIP,
+	# Draw edges
+	pyglet.gl.glColor4f(*edgeColor)
+	pyglet.graphics.draw(numEdges, pyglet.gl.GL_LINE_STRIP,
     	('v2f', points)
 	)
-
 	# Draw points
 	pyglet.gl.glColor4f(0.0,1.0,0.0,1.0)
 	pyglet.graphics.draw(numEdges, pyglet.gl.GL_POINTS,
@@ -83,13 +84,43 @@ def drawGrid():
 				xoffset = innerRadius
 			drawHex(False, (startX+xoffset+x*2*innerRadius, startY+y*(radius*1.5)), radius)
 
+def drawFittedGrid(hexesInRow=10):
+	# Width of hexagons (w=root3*Radius/2) is calculated from screenwidth, which then determines hex radius
+	hexWidthRadius = (screenWidth / hexesInRow) / 2
+	hexRadius = (2*hexWidthRadius) / math.sqrt(3) # Also edge length
+	# Loop over hex coordinate locations
+	hexCentreY = 0
+	rowNumber = 0
+	while hexCentreY - hexRadius < screenHeight:
+		hexCentreX = 0 
+		# Offset each row to allow for tesselation
+		if rowNumber%2==1:
+			hexCentreX += hexWidthRadius
+		# Draw all hexagons in row
+		colNumber = 0
+		while hexCentreX - hexWidthRadius < screenWidth:
+			hexPolygon = hexagon.Hexagon((hexCentreX, hexCentreY), hexRadius)
+			if rowNumber == 0:
+				# Full hex must be drawn as West side will be seen
+				hexPolygon.drawHex(True)
+			else:
+				hexPolygon.drawHex(False)
+			#drawHex(False, (hexCentreX, hexCentreY), hexRadius, hexWidthRadius, (1.0, 0.0, 0.0, 1.0))
+			hexCentreX += 2 * hexWidthRadius
+			colNumber += 1
+		rowNumber += 1
+		hexCentreY += hexRadius * 1.5
+
 @window.event
 def on_draw():
 	window.clear()
 	#Draw centre
 	cX = 250
 	cY = 250
-	drawGrid()
+	#drawGrid()
+	drawFittedGrid()
+	#hexPolygon = hexagon.Hexagon((100, 100), 30)
+	#hexPolygon.drawHex()
 	#image.blit(0, 0)
 
 print("Running app")
