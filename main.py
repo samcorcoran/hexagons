0f7c9a8111fbd7f4b2b8ec2bac16b4d6f20d233a
RedSpaceman <samuel.corcoran+github@gmail.com>
2013-08-20 00:51:55 +0100

    Moved altitude-setting functions into a new module called terrain

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import math
 import copy
 
 import graph
+import terrain
 import regions
 
 window = pyglet.window.Window()
@@ -91,64 +92,6 @@ def createHexGridFromPoints(hexesInRow=10, clipPointsToScreen=True):
 		hexCentreY += hexRadius * 1.5
 	print("Created a hexGrid with %d rows. Odd rows are length %d and even are %d." % (len(gridRows), len(gridRows[0]), len(gridRows[1])))
 	return gridRows
-
-def assignHexMapAltitudes(hexMap):
-	# Iterate over hexes in may
-	for nextHex in hexMap.values():
-		# Iterate over hex's points
-		cumulativeAltitude = 0
-		for point in nextHex.points:
-			# If point has no altitude, assign one randomly
-			if not point.altitude:
-				point.altitude = random.uniform(0.1,1)
-				#print("Assigning altitude: %f" % (point.altitude))
-			cumulativeAltitude += point.altitude
-		# After all perimeter points are complete, take average for hex centre altitude
-		nextHex.centre.altitude = cumulativeAltitude/len(nextHex.points)
-		#print("Hex %s centre altitude is %f" % (str(nextHex.hexIndex), nextHex.centre.altitude))
-
-def assignHexMapAltitudesFromCoast(hexRegion):
-	minimumAltitude = 0.1
-	for nextHex in hexRegion.hexes.values():
-		#print("Altitudes for hex %s " % (str(nextHex.hexIndex)))
-		altitudes = []
-		for point in nextHex.points:
-			if not point.altitude:
-				distanceFromCoast = hexRegion.hexBorderDistances[nextHex.hexIndex]+1
-				# Plus one to offset zero indexing, plus another to prevent altitudes of 1
-				largestDist = len(hexRegion.borderHexes)+1
-				#print("Altitude: %f/%f" % (distanceFromCoast, largestDist)) 
-				point.altitude = 0 if largestDist == 0 else (distanceFromCoast**2)/(largestDist**2)
-				# Add some randomness
-				point.altitude *= random.triangular(0.5, 1.5, 1)
-				point.altitude += minimumAltitude
-			altitudes.append( point.altitude )
-		nextHex.centre.altitude = sum(altitudes)/len(altitudes)
-		#print("  Altitudes: %s, centre: %s" % (str(altitudes), str(nextHex.centre.altitude)))
-
-def assignRegionVertexAltitudesFromCoast(hexRegion):
-	minimumAltitude = 0.1
-	for nextHex in hexRegion.hexes.values():
-		#print("Altitudes for hex %s " % (str(nextHex.hexIndex)))
-		altitudes = []
-		for point in nextHex.points:
-			if not point.altitude:
-				closestBorderVertex = hexRegion.vertexBorderDistances[(point.x, point.y)]
-				distanceFromCoast = point.distanceFrom(closestBorderVertex)
-				#print("Altitude: %f/%f" % (distanceFromCoast, largestDist))
-				point.altitude = 0 if hexRegion.largestVertexBorderDistance == 0 else (distanceFromCoast**2)/(hexRegion.largestVertexBorderDistance**2)
-				# Add some randomness
-				point.altitude *= random.uniform(0.5, 1.5)
-				point.altitude += minimumAltitude
-			altitudes.append( point.altitude )
-		nextHex.centre.altitude = sum(altitudes)/len(altitudes)
-		#print("  Altitudes: %s, centre: %s" % (str(altitudes), str(nextHex.centre.altitude)))
-
-def assignEqualAltitudes(hexMap):
-	for nextHex in hexMap.values():
-		for point in nextHex.points:
-			point.altitude = 1
-		nextHex.centre.altitude = 1
 
 def screenClipGridHexagons(hexGrid):
 	# Loop over boundary hexes, fixing their off-screen points to the screen boundary
@@ -281,7 +224,7 @@ def drawDrainageRoutes(hexMap):
 def on_draw():
 	global gridChanged
 	global hexGrid
-	hexesInRow = 50
+	hexesInRow = 20
 	if gridChanged:
 		window.clear()
 		if True:
@@ -304,10 +247,10 @@ def on_draw():
 			islandRegion.calculateAllVertexBorderDistances()
 
 			# Assign heights to land vertices
-			#assignEqualAltitudes(landHexes)
-			#assignHexMapAltitudes(landHexes)
-			#assignHexMapAltitudesFromCoast(islandRegion)
-			assignRegionVertexAltitudesFromCoast(islandRegion)
+			#terrain.assignEqualAltitudes(landHexes)
+			#terrain.assignHexMapAltitudes(landHexes)
+			#terrain.assignHexMapAltitudesFromCoast(islandRegion)
+			terrain.assignRegionVertexAltitudesFromCoast(islandRegion)
 
 		gridChanged = False
 	drawHexGrid(hexGrid, drawHexEdges=False, drawHexFills=True, drawHexCentres=False)
---
+++ terrain.py
@@ -0,0 +1,64 @@
+import math
+import random
+
+import graph
+import regions
+import drawUtils
+
+def assignHexMapAltitudes(hexMap):
+	# Iterate over hexes in may
+	for nextHex in hexMap.values():
+		# Iterate over hex's points
+		cumulativeAltitude = 0
+		for point in nextHex.points:
+			# If point has no altitude, assign one randomly
+			if not point.altitude:
+				point.altitude = random.uniform(0.1,1)
+				#print("Assigning altitude: %f" % (point.altitude))
+			cumulativeAltitude += point.altitude
+		# After all perimeter points are complete, take average for hex centre altitude
+		nextHex.centre.altitude = cumulativeAltitude/len(nextHex.points)
+		#print("Hex %s centre altitude is %f" % (str(nextHex.hexIndex), nextHex.centre.altitude))
+
+def assignHexMapAltitudesFromCoast(hexRegion):
+	minimumAltitude = 0.1
+	for nextHex in hexRegion.hexes.values():
+		#print("Altitudes for hex %s " % (str(nextHex.hexIndex)))
+		altitudes = []
+		for point in nextHex.points:
+			if not point.altitude:
+				distanceFromCoast = hexRegion.hexBorderDistances[nextHex.hexIndex]+1
+				# Plus one to offset zero indexing, plus another to prevent altitudes of 1
+				largestDist = len(hexRegion.borderHexes)+1
+				#print("Altitude: %f/%f" % (distanceFromCoast, largestDist)) 
+				point.altitude = 0 if largestDist == 0 else (distanceFromCoast**2)/(largestDist**2)
+				# Add some randomness
+				point.altitude *= random.triangular(0.5, 1.5, 1)
+				point.altitude += minimumAltitude
+			altitudes.append( point.altitude )
+		nextHex.centre.altitude = sum(altitudes)/len(altitudes)
+		#print("  Altitudes: %s, centre: %s" % (str(altitudes), str(nextHex.centre.altitude)))
+
+def assignRegionVertexAltitudesFromCoast(hexRegion):
+	minimumAltitude = 0.1
+	for nextHex in hexRegion.hexes.values():
+		#print("Altitudes for hex %s " % (str(nextHex.hexIndex)))
+		altitudes = []
+		for point in nextHex.points:
+			if not point.altitude:
+				closestBorderVertex = hexRegion.vertexBorderDistances[(point.x, point.y)]
+				distanceFromCoast = point.distanceFrom(closestBorderVertex)
+				#print("Altitude: %f/%f" % (distanceFromCoast, largestDist))
+				point.altitude = 0 if hexRegion.largestVertexBorderDistance == 0 else (distanceFromCoast**2)/(hexRegion.largestVertexBorderDistance**2)
+				# Add some randomness
+				point.altitude *= random.uniform(0.5, 1.5)
+				point.altitude += minimumAltitude
+			altitudes.append( point.altitude )
+		nextHex.centre.altitude = sum(altitudes)/len(altitudes)
+		#print("  Altitudes: %s, centre: %s" % (str(altitudes), str(nextHex.centre.altitude)))
+
+def assignEqualAltitudes(hexMap):
+	for nextHex in hexMap.values():
+		for point in nextHex.points:
+			point.altitude = 1
+		nextHex.centre.altitude = 1
